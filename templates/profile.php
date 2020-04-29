2dfba5949cb99d5d4932c117eacd02589ea4d1e2
Vadim Shmakov <shmakovvadim@mail.ru>
2020-04-29 15:42:37 +0500

    [profile] Refactored template

--- templates/profile.php
+++ templates/profile.php
@@ -4,28 +4,24 @@
             <th>Account amount</th>
             <td><?= $amount ?>$</td>
         </tr>
-        <tr>
-            <th>Withdraw</th>
-            <td>
-                <form method="post">
-                    <ul>
-                        <?php foreach ($errors as $error): ?>
-<li><?= $error ?></li>
-                        <?php endforeach ?>
-                    </ul>
-                    <div>
-                        <input
-                                type="number"
-                                min="0"
-                                step="any"
-                                name="withdraw"
-                                value="<?= htmlspecialchars($withdraw) ?>">$
-                    </div>
-                    <div>
-                        <input type="submit" value="Apply">
-                    </div>
-                </form>
-            </td>
-        </tr>
     </table>
-</div>
+
+    <form method="post">
+        <ul>
+            <?php foreach ($errors as $error): ?>
+                <li><?= $error ?></li>
+            <?php endforeach ?>
+        </ul>
+        <div>
+            <input
+                    type="number"
+                    min="0"
+                    step="any"
+                    name="withdraw"
+                    value="<?= htmlspecialchars($withdraw) ?>">$
+        </div>
+        <div>
+            <input type="submit" value="Withdraw">
+        </div>
+    </form>
+        </div>
